2073d3f6b2a7220fd0294a87851ebb97da6712ba
Matheus <matheusleao585@gmail.com>
2025-02-11 15:21:44 -0300

    adicionando lógicas typescript

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-response.type';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private HttpClient: HttpClient) { }
+
+  login(namw:string, password:string){
+    return this.HttpClient.post<LoginResponse>("/login", {name:namw, password}).pipe(
+      tap((value) =>{
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      }
+    ))
+  }
+}
